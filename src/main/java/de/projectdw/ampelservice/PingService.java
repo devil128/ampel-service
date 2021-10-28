8925d3dea0ce8e71a34328e4e60afdd443439c68
devil1281 <darvin.w@freenet.de>
2021-10-28 09:57:06 +0200

    fixed wrong url

--- src/main/java/de/projectdw/ampelservice/PingService.java
+++ src/main/java/de/projectdw/ampelservice/PingService.java
@@ -4,6 +4,7 @@ package de.projectdw.ampelservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
@@ -15,7 +16,7 @@ public class PingService {
     UserLogsRepository userLogsRepository;
 
     @GetMapping("/ping")
-    public String getPing(@Value("Test") String name) {
+    public String getPing(@RequestParam(required = true) String name) {
         UserLog log = new UserLog();
         log.setSuccess(true);
         log.setTimestamp(new Date().toInstant().toEpochMilli() + "");
@@ -23,6 +24,13 @@ public class PingService {
         userLogsRepository.save(log);
         return "failed";
     }
+
+    @GetMapping("/clear")
+    public String clear(){
+        userLogsRepository.deleteAll(userLogsRepository.findAll());
+        return "Worked";
+    }
+
 
     @GetMapping("/getFailed")
     public List<UserLog> getFailed() {
