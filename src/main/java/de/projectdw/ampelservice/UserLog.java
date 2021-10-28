61c4e9c4d9f8446878fdfe9459a93457a6fd35ac
devil1281 <darvin.w@freenet.de>
2021-10-28 22:19:53 +0200

    fixed dockerfile

--- src/main/java/de/projectdw/ampelservice/UserLog.java
+++ src/main/java/de/projectdw/ampelservice/UserLog.java
@@ -1,11 +1,15 @@
 package de.projectdw.ampelservice;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
 
 @Data
 @Document(collection = "pings")
 public class UserLog {
+    @MongoId
+    private String id;
     String username;
     String timestamp;
     boolean success;
